b4b829d4ab5972dc9516b385c076a099c2cad8db
Victor Saraiva <victor_alex2005@hotmail.com>
2024-12-31 18:21:16 -0300

    entidades

--- src/main/kotlin/com/kotlinwebapp/bookstore/domain/Author.kt
+++ src/main/kotlin/com/kotlinwebapp/bookstore/domain/Author.kt
@@ -1,3 +1,24 @@
 package com.kotlinwebapp.bookstore.domain
 
-data class Author(var id: Long?, var name: String, var age: Int, var description: String, var image: String)
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "authors")
+data class Author(
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
+    var id: Long?,
+
+    @Column(name = "name")
+    var name: String,
+
+    @Column(name = "age")
+    var age: Int,
+
+    @Column(name = "description")
+    var description: String,
+
+    @Column(name = "image")
+    var image: String
+)
--- src/main/kotlin/com/kotlinwebapp/bookstore/domain/Book.kt
+++ src/main/kotlin/com/kotlinwebapp/bookstore/domain/Book.kt
@@ -1,3 +1,23 @@
 package com.kotlinwebapp.bookstore.domain
 
-data class Book(var isbn: String, var title: String, var description: String, var image: String, var author: Author)
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "books")
+data class Book(
+    @Id @Column(name = "isbn")
+    var isbn: String,
+
+    @Column(name = "title")
+    var title: String,
+
+    @Column(name = "description")
+    var description: String,
+
+    @Column(name = "image")
+    var image: String,
+
+    @ManyToOne(cascade = [CascadeType.DETACH])
+    @JoinColumn(name = "author_id")
+    val author: Author
+)
