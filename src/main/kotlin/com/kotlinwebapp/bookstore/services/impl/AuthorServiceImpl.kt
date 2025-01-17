5488e178541ab3ee6f9da4c4b82f62ef134f7485
Victor Saraiva <victor_alex2005@hotmail.com>
2025-01-17 14:03:18 -0300

    feat : base para implementação do authorService

---
+++ src/main/kotlin/com/kotlinwebapp/bookstore/services/impl/AuthorServiceImpl.kt
@@ -0,0 +1,10 @@
+package com.kotlinwebapp.bookstore.services.impl
+
+import com.kotlinwebapp.bookstore.domain.entities.AuthorEntity
+import com.kotlinwebapp.bookstore.services.AuthorService
+
+class AuthorServiceImpl  : AuthorService {
+    override fun save(authorEntity: AuthorEntity) {
+        TODO("Not yet implemented")
+    }
+}
