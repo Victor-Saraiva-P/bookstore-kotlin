e775b778cf7c156903c584aa6481f60136ff028e
Victor Saraiva <victor_alex2005@hotmail.com>
2025-01-17 13:54:41 -0300

    feat : criando extensão para poder fazer a conversão de dtos unficada num arquivo só

---
+++ src/main/kotlin/com/kotlinwebapp/bookstore/Extensions.kt
@@ -0,0 +1,24 @@
+package com.kotlinwebapp.bookstore
+
+import com.kotlinwebapp.bookstore.domain.dto.AuthorDto
+import com.kotlinwebapp.bookstore.domain.entities.AuthorEntity
+
+fun AuthorEntity.toAuthorDto(): AuthorDto {
+    return AuthorDto(
+        id = this.id,
+        name = this.name,
+        image = this.image,
+        description = this.description,
+        age = this.age,
+    )
+}
+
+fun AuthorDto.toAuthorEntity(): AuthorEntity {
+    return AuthorEntity(
+        id = this.id,
+        name = this.name,
+        image = this.image,
+        description = this.description,
+        age = this.age,
+    )
+}
