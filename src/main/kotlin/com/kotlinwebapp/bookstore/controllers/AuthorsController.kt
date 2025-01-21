0117a56062cfa2f7acf9f5ca066c97b5d577c460
Victor Saraiva <victor_alex2005@hotmail.com>
2025-01-20 22:17:14 -0300

    fix : quando um author é criado seu código de reponse é passado corretamente

--- src/main/kotlin/com/kotlinwebapp/bookstore/controllers/AuthorsController.kt
+++ src/main/kotlin/com/kotlinwebapp/bookstore/controllers/AuthorsController.kt
@@ -5,6 +5,8 @@ import com.kotlinwebapp.bookstore.domain.entities.AuthorEntity
 import com.kotlinwebapp.bookstore.services.AuthorService
 import com.kotlinwebapp.bookstore.toAuthorDto
 import com.kotlinwebapp.bookstore.toAuthorEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController
 class AuthorsController(private val authorService: AuthorService) {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(@RequestBody authorDto: AuthorDto): AuthorDto {
-        return authorService.save(
+    fun createAuthor(@RequestBody authorDto: AuthorDto): ResponseEntity<AuthorDto> {
+        val createdAuthor = authorService.save(
             authorDto.toAuthorEntity()
         ).toAuthorDto()
+
+        return ResponseEntity(createdAuthor, HttpStatus.CREATED)
     }
 }
