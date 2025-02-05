24dc7b7b0e2be9eb7d4a19f413501ab42fc2899a
Victor Saraiva <victor_alex2005@hotmail.com>
2025-02-05 16:11:17 -0300

    test: adicionando testes para o partial update

--- src/main/kotlin/com/kotlinwebapp/bookstore/domain/AuthorUpdateRequest.kt
+++ src/main/kotlin/com/kotlinwebapp/bookstore/domain/AuthorUpdateRequest.kt
@@ -1,14 +1,9 @@
 package com.kotlinwebapp.bookstore.domain
 
-import jakarta.persistence.Column
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-
-class AuthorUpdateRequest(
-    val id: Long?,
-    val name: String?,
-    val age: Int?,
-    val description: String?,
-    val image: String?
+data class AuthorUpdateRequest(
+    val id: Long? = null,
+    val name: String? = null,
+    val age: Int? = null,
+    val description: String? = null,
+    val image: String? = null
 )
--- src/test/kotlin/com/kotlinwebapp/bookstore/TestDataUtil.kt
+++ src/test/kotlin/com/kotlinwebapp/bookstore/TestDataUtil.kt
@@ -1,14 +1,16 @@
 package com.kotlinwebapp.bookstore
 
+import com.kotlinwebapp.bookstore.domain.AuthorUpdateRequest
 import com.kotlinwebapp.bookstore.domain.dto.AuthorDto
+import com.kotlinwebapp.bookstore.domain.dto.AuthorUpdateRequestDto
 import com.kotlinwebapp.bookstore.domain.entities.AuthorEntity
 
 fun testAuthorDtoA(id: Long? = null) = AuthorDto(
     id = id,
-    name = "nome de teste",
+    name = "nome de teste A",
     age = 404,
-    description = "descricao",
-    image = "author-image.jpeg",
+    description = "descricao do A",
+    image = "author-imageA.jpeg",
 )
 
 fun testAuthorEntityA(id: Long? = null) = AuthorEntity(
@@ -25,5 +27,21 @@ fun testAuthorEntityB(id: Long? = null) = AuthorEntity(
     age = 404,
     description = "descricao do B",
     image = "author-imageB.jpeg",
+)
+
+fun testAuthorUpdateRequestDtoA(id: Long? = null) = AuthorUpdateRequestDto(
+    id = id,
+    name = "nome de teste A",
+    age = 404,
+    description = "descricao do A",
+    image = "author-imageA.jpeg",
+)
+
+fun testAuthorUpdateRequestA(id: Long? = null) = AuthorUpdateRequest(
+    id = id,
+    name = "nome de teste A",
+    age = 404,
+    description = "descricao do A",
+    image = "author-imageA.jpeg",
 )
 
