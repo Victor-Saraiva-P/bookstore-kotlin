0dcd231b85f878a456a272c1e2917c3b026546e0
Victor Saraiva <victor_alex2005@hotmail.com>
2025-02-17 17:05:41 -0300

    test: adicionando testes para o createUpdate dos Books

---
+++ src/test/kotlin/com/kotlinwebapp/bookstore/services/impl/BookServiceImplTest.kt
@@ -0,0 +1,64 @@
+package com.kotlinwebapp.bookstore.services.impl
+
+import com.kotlinwebapp.bookstore.*
+import com.kotlinwebapp.bookstore.domain.AuthorSummary
+import com.kotlinwebapp.bookstore.repositories.AuthorRepository
+import com.kotlinwebapp.bookstore.repositories.BookRepository
+import jakarta.transaction.Transactional
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+
+@SpringBootTest
+@Transactional
+class BookServiceImplTest @Autowired constructor(
+    private val underTest: BookServiceImpl,
+    private val bookRepository: BookRepository,
+    private val authorRepository: AuthorRepository
+) {
+
+    @Test
+    fun `test que createUpdate throw IllegalStateException quando o author nao existir`() {
+        val authorSummary = AuthorSummary(id = 999L)
+        val bookSummary = testBookSummaryA(BOOK_A_ISBN, authorSummary)
+        assertThrows<IllegalStateException> {
+            underTest.createUpdate(BOOK_A_ISBN, bookSummary)
+        }
+    }
+
+    @Test
+    fun `test que o createUpdate com sucesso create um book no database`() {
+        val savedAuthor = authorRepository.save(testAuthorEntityA())
+        assertThat(savedAuthor).isNotNull()
+
+        val authorSummary = AuthorSummary(id = savedAuthor.id!!)
+        val bookSummary = testBookSummaryA(BOOK_A_ISBN, authorSummary)
+        val (savedBook, isCreated) = underTest.createUpdate(BOOK_A_ISBN, bookSummary)
+        assertThat(savedBook).isNotNull()
+
+        val recaledBook = bookRepository.findByIdOrNull(BOOK_A_ISBN)
+        assertThat(recaledBook).isNotNull().isEqualTo(savedBook)
+        assertThat(isCreated).isTrue()
+    }
+
+    @Test
+    fun `test que o createUpdate com sucesso update um book no database`() {
+        val savedAuthor = authorRepository.save(testAuthorEntityA())
+        assertThat(savedAuthor).isNotNull()
+
+        val savedBook = bookRepository.save(testBookEntityA(BOOK_A_ISBN, savedAuthor))
+        assertThat(savedBook).isNotNull()
+
+        val authorSummary = AuthorSummary(id = savedAuthor.id!!)
+        val bookSummary = testBookSummaryB(BOOK_A_ISBN, authorSummary)
+        val (updatedBook, isCreated) = underTest.createUpdate(BOOK_A_ISBN, bookSummary)
+        assertThat(updatedBook).isNotNull()
+
+        val recaledBook = bookRepository.findByIdOrNull(BOOK_A_ISBN)
+        assertThat(recaledBook).isNotNull()
+        assertThat(isCreated).isFalse()
+    }
+}
