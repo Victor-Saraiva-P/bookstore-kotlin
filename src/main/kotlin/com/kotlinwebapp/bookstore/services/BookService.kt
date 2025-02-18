f26f8d5f147a617dfb69c63cd1d2c563ac0d1a91
Victor Saraiva <victor_alex2005@hotmail.com>
2025-02-18 15:29:05 -0300

    feat : implementação do read many books

--- src/main/kotlin/com/kotlinwebapp/bookstore/services/BookService.kt
+++ src/main/kotlin/com/kotlinwebapp/bookstore/services/BookService.kt
@@ -7,4 +7,5 @@ import com.kotlinwebapp.bookstore.domain.entities.BookEntity
 
 interface BookService {
     fun createUpdate(isbn: String, bookSummary: BookSummary): Pair<BookEntity, Boolean>
+    fun list():List<BookEntity>
 }
